09a700421f3b25d49cc1077dbe16054ec729da80
chinhtrung <chinhtrung596@gmail.com>
2021-06-02 13:22:19 -0400

    changing style

--- app.js
+++ app.js
@@ -45,6 +45,163 @@
 
 // targetRandomKey();
 
+
+
+
+var keyData = {
+    q: {
+        sound: new Howl({
+          urls: ['./sounds/bubbles.mp3']
+        })
+    },
+    w: {
+        sound: new Howl({
+          urls: ['./sounds/clay.mp3']
+        })
+    },
+    e: {
+        sound: new Howl({
+          urls: ['./sounds/confetti.mp3']
+        })
+    },
+    r: {
+        sound: new Howl({
+          urls: ['./sounds/corona.mp3']
+        })
+    },
+        t: {
+        sound: new Howl({
+          urls: ['./sounds/dotted-spiral.mp3']
+        })
+    },
+    y: {
+        sound: new Howl({
+          urls: ['./sounds/flash-1.mp3']
+        })
+    },
+    u: {
+        sound: new Howl({
+          urls: ['./sounds/flash-2.mp3']
+        })
+    },
+    i: {
+        sound: new Howl({
+          urls: ['./sounds/flash-3.mp3']
+        })
+    },
+    o: {
+        sound: new Howl({
+            urls: ['./sounds/glimmer.mp3']
+        })
+    },
+    p: {
+        sound: new Howl({
+          urls: ['./sounds/moon.mp3']
+        })
+    },
+    a: {
+        sound: new Howl({
+          urls: ['./sounds/pinwheel.mp3']
+        })
+    },
+    s: {
+        sound: new Howl({
+          urls: ['./sounds/piston-1.mp3']
+        })
+    },
+        d: {
+        sound: new Howl({
+          urls: ['./sounds/piston-2.mp3']
+        })
+    },
+    f: {
+        sound: new Howl({
+          urls: ['./sounds/prism-1.mp3']
+        })
+    },
+    g: {
+        sound: new Howl({
+          urls: ['./sounds/prism-2.mp3']
+        })
+    },
+    h: {
+        sound: new Howl({
+          urls: ['./sounds/prism-3.mp3']
+        })
+    },
+    j: {
+        sound: new Howl({
+          urls: ['./sounds/splits.mp3']
+        })
+    },
+    k: {
+        sound: new Howl({
+          urls: ['./sounds/squiggle.mp3']
+        })
+    },
+    l: {
+        sound: new Howl({
+          urls: ['./sounds/strike.mp3']
+        })
+    },
+    z: {
+        sound: new Howl({
+          urls: ['./sounds/suspension.mp3']
+        })
+    },
+    x: {
+        sound: new Howl({
+          urls: ['./sounds/timer.mp3']
+        })
+    },
+    c: {
+        sound: new Howl({
+          urls: ['./sounds/ufo.mp3']
+        })
+    },
+    v: {
+        sound: new Howl({
+          urls: ['./sounds/veil.mp3']
+        })
+    },
+    b: {
+        sound: new Howl({
+          urls: ['./sounds/wipe.mp3']
+        })
+    },
+    n: {
+        sound: new Howl({
+            urls: ['./sounds/zig-zag.mp3']
+        })
+    },
+    m: {
+        sound: new Howl({
+          urls: ['./sounds/moon.mp3']
+        })
+    }
+}
+
+let choose = document.querySelector(".key.white.C.octave4");
+    
+choose.addEventListener('mouseover', ()=>{
+    console.log("I'M HOVERED");
+})
+
+var eventT = new MouseEvent('mouseover', {
+    'view': window,
+    'bubbles': true,
+    'cancelable': true
+});
+
 document.onkeydown = function (e) {
-    console.dir(e);
+    // console.dir(e);
+    if(keyData[e.key]){
+        keyData[e.key].sound.play();
+    }
+  
+    console.dir(choose);
+
+    choose.dispatchEvent(eventT);
 };
+
+

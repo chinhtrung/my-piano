e1f36442dae86c08fbb32d94d5f605925d1feaf0
chinhtrung <chinhtrung596@gmail.com>
2021-06-05 04:37:52 -0400

    move to util

--- util/convertFormat.sh
+++ util/convertFormat.sh
@@ -15,7 +15,7 @@ pwd
 # }' allFile.txt
 
 awk -F. '{
-    from = "./pianoSounds/uiowa/aiff_format/" $0
-    to = "./pianoSounds/uiowa/m4a_format/" $3 ".m4a"
+    from = "../pianoSounds/uiowa/aiff_format/" $0
+    to = "../pianoSounds/uiowa/m4a_format/" $3 ".m4a"
     system("ffmpeg -i " from " -af loudnorm,silenceremove=start_periods=1:start_silence=0.05:start_threshold=-40dB,afade=out:st=3:d=1.5,afade=in:st=0:d=0.05 -to 4.5 " to)
 }' allFile.txt

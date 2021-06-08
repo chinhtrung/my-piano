1dd784f3e96f633257c3c65e767fdedaa81afad4
chinhtrung <chinhtrung596@gmail.com>
2021-06-07 22:37:10 -0400

    finish add server

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ app.use(express.static(path.join(__dirname, 'files')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-let count = 0;
 app.get('/the_simple_piano', (req,res)=>{
-    count += 1;
     console.log("NEW REQUEST");
-    console.log("Count load", count);
     res.render('home', {dirPath: __dirname});
 })
 

1b8837965ac4e27afb2ae6e28c50fb4ee054b5d4
chinhtrung <chinhtrung596@gmail.com>
2021-07-02 00:32:21 -0400

    remove 'file' path

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'files')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
